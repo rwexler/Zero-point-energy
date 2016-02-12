0cf150b6db271cb43d62f0f10ccf0194f945f891
rwexler <rwexler@sas.upenn.edu>
2016-02-12 14:43:48 -0500

    Modified code to handle Gamma-point calculations

--- zpe.py
+++ zpe.py
@@ -1,38 +1,57 @@
 #Read input filename
-inputname = raw_input("Input filename: ")
-input = open(inputname,"r")
-for line in input:
-    if "/" in line:
+isGamma = raw_input("y or n? ")
+if isGamma == "y":
+    nq = 1
+if isGamma == "n":
+    inputname = raw_input("Input filename: ")
+    input = open(inputname,"r")
+    for line in input:
+        if "/" in line:
+            break
+    #Get number of q-points
+    for line in input:
+        list = line.split()
+        nq = int(list[0])
         break
-#Get number of q-points
-for line in input:
-    list = line.split()
-    nq = int(list[0])
-    break
-#Get weights for each q-points
-weights = []
-for line in input:
-    list = line.split()
-    weights.append(float(list[3]))
+    #Get weights for each q-points
+    weights = []
+    for line in input:
+        list = line.split()
+        weights.append(float(list[3]))
 #Get frequencies
 freqs = []
-for i in range(nq):
-    qn = []
-    filename = "dyn"+str(i+1)
+if isGamma == "y":
+    filename = "dyn.G"
     file = open(filename,"r")
     for line in file:
         if "freq" in line:
             list = line.split()
-            qn.append(float(list[7]))
-    freqs.append(qn)
+            freqs.append(float(list[7]))
     file.close()
+if isGamma == "n":
+    for i in range(nq):
+        qn = []
+        filename = "dyn"+str(i+1)
+        file = open(filename,"r")
+        for line in file:
+            if "freq" in line:
+                list = line.split()
+                qn.append(float(list[7]))
+        freqs.append(qn)
+        file.close()
 #Calculate zero point energy
 tsum = 0
 wpeV = 8065.54429
-for i in range(len(freqs)): #sum over q-points
-    qsum = 0
-    for j in range(len(qn)): #sum over frequencies
-        qsum += freqs[i][j]
-    qsum = 1/wpeV/2.0*qsum
-    tsum += weights[i]*qsum
-print tsum/sum(weights)
+if isGamma == "y":
+    for i in range(len(freqs)):
+        tsum += freqs[i]
+    tsum = 1/wpeV/2.0*tsum
+    print tsum
+if isGamma == "n":
+    for i in range(len(freqs)): #sum over q-points
+        qsum = 0
+        for j in range(len(qn)): #sum over frequencies
+            qsum += freqs[i][j]
+        qsum = 1/wpeV/2.0*qsum
+        tsum += weights[i]*qsum
+    print tsum/sum(weights)
